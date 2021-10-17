cd2e73900fec785e9955f266f5db45085d8dbe42
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-10-17 18:59:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,24 @@
 	//target.innerText = document.id_form1.id_textBox1.value;//これでもOK
 }
 */
-
 /*
 fetch('https://ipinfo.io?callback')
   .then(res => res.json())
   //.then(json => console.log(json.ip))
   .then(json => document.write(json.ip))
 */
-
+/*
 fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:9784043636037')
   .then(res => res.json())
   //.then(json => console.log(json.ip))
   .then(json => document.write(json.items))
+*/
+
+//検索語句
+var words = 9784043636037;
+// 検索 API を叩く
+var res = await fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:"+words);
+// JSON に変換
+var data = await res.json();
+//表示
+document.write(data);
