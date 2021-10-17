246a172d1ee8966e170bdb170b27810bcd78ca2f
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-10-17 18:32:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-function onButtonClick() {
+/*function onButtonClick() {
 	target = document.getElementById("output");
 	target.innerText = document.forms.id_form1.id_textBox1.value;
 	//target.innerText = document.id_form1.id_textBox1.value;//これでもOK
 }
+*/
+
 /*
-let search = document.(search_box).(search_botton)
 fetch('https://ipinfo.io?callback')
   .then(res => res.json())
   //.then(json => console.log(json.ip))
   .then(json => document.write(json.ip))
 */
+
+fetch('https://www.youtube.com/watch?v=GSpwTvYgFrM')
+  .then(res => res.json())
+  //.then(json => console.log(json.ip))
+  .then(json => document.write(json.items))
