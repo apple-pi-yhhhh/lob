b9b079fe2cb43960052efbc50f0bd0a9ed35563e
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-10-17 13:17:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+function onButtonClick() {
+	target = document.getElementById("output");
+	target.innerText = document.forms.id_form1.id_textBox1.value;
+	//target.innerText = document.id_form1.id_textBox1.value;//これでもOK
+}
+/*
 let search = document.(search_box).(search_botton)
 fetch('https://ipinfo.io?callback')
   .then(res => res.json())
   //.then(json => console.log(json.ip))
   .then(json => document.write(json.ip))
+*/
