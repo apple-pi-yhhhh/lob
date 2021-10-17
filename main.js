a6f56d110798308070be6ba8d956fc6c8943904a
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-10-18 08:24:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,20 @@
 fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:9784101010137')
   .then(response => response.json())
   .then(data => console.log(data))
-  .then(data => console.log(data.items[0]));
+  //.then(data => console.log(data.items[0]));
 //document.wite(data.items.0.volumeInfo.title)
+var target = ['title'];
+var result = {};
+for(var key of Object.keys(data)){
+    // 連想配列のキーと配列の値が一致するか検索
+    for(var i of target){
+        if(key == i){
+            result[key] = data[key]; // 連想配列に格納
+            break;
+        }
+    }
+}
+console.log(result);
 /*
 asyncCall();
 async function asyncCall() {
