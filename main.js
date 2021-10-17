03854b0a90f7207af1a82af5ed71ab736e03bcf3
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-10-17 23:21:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
 fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:9784101010137')
   .then(response => response.json())
   .then(data => console.log(data));
+  .then(data => document.write(data));
 /*
 asyncCall();
 async function asyncCall() {
