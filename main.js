a808e2ef0abc29d213d06b7554fdf3baaf476713
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-10-18 10:59:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var data = {
     "いちご":{"price":"400", "num":"1"},
     "ぶどう":{"price":"380", "num":"7"}
 }
-var target = ['title'];
+var target = ['みかん'];
 var result = {};
 for(var key of Object.keys(data)){
     // 連想配列のキーと配列の値が一致するか検索
