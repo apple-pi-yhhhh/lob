3e6c63e0604390b7667427b109e24a59eadef736
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-10-19 16:18:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:9784101010137')
 .then (data => {
     //console.log(data);
     //console.log(data.items[0].volumeInfo.title);
-    document.write(data.items.[0].volumeInfo.title);
+    document.write("本のタイトルは、「"+ data.items.[0].volumeInfo.title +"」だお！");
 });
 //document.wite(data.items.0.volumeInfo.title)
