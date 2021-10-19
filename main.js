c759d79b10d497d745c12711f92879d48a28df13
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-10-19 16:02:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:9784101010137')
   .then(response => response.json())
 .then (data => {
-    console.log(data);
-    console.log(data.items[0].volumeInfo.title);
+    //console.log(data);
+    //console.log(data.items[0].volumeInfo.title);
     document.wite(data.items.[0].volumeInfo.title);
 });
 //document.wite(data.items.0.volumeInfo.title)
