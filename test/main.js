0bcfe25bee251f18a1d86af34efecb0d470bf9a4
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-10-19 21:29:13 +0900

    Update main.js

--- test/main.js
+++ test/main.js
@@ -1,13 +1,14 @@
 let num_books = all_books.length;
 for (  var i = 0;  i < num_books;  i++  ) {
 	
-	fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:9784101010137')
+	fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:'+ all_books[i])
 	.then(response => response.json())
 	.then (data => {
-   	 console.log(data);
-   	 console.log(data.items[0].volumeInfo.title);
+   		console.log(i +"つ目の本は「"+ data.items[0].volumeInfo.title +"」です。")
+		//console.log(data);
+   		//console.log(data.items[0].volumeInfo.title);
 	});
 	
-	console.log(i);
+	//console.log(i);
 }
 console.log(all_books);
