60957343c8cd5868d379c2c5950a01638c09a83e
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2022-01-19 09:10:11 +0900

    Update login.js

--- op/login.js
+++ op/login.js
@@ -1,10 +1,10 @@
-let cookie_arr = document.cookie.split('; ');
-let cookies = {};
-for (let i = 0; i < cookies.length; i++) {
-  cookie_arr[i].sprit("=");
-  cookies[cookie_arr[i][0]] = cookie_arr[i][1]
+function cookie_get(cookies){
+  let cookie_arr = document.cookie.split('; ');
+  for (let i = 0; i < cookies.length; i++) {
+    cookie_arr[i].sprit("=");
+    cookies[cookie_arr[i][0]] = cookie_arr[i][1]
+  }
 }
-
 function login() {
   cookie_make("mail", document.getElementById("e-mail").value);
   cookie_make("pass", document.getElementById("pass").value);
