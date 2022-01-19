11cc4b7834157a7aa9cf7e1fa4752f4578af0365
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2022-01-19 12:54:36 +0900

    Update login.js

--- op/login.js
+++ op/login.js
@@ -28,21 +28,24 @@ function user_show(){
   ]
   let html2 = [
     '<div id="user_show">',
-    'メールアドレス：<p><input type="text" id="e-mail">@jhs.gs.chiba-u.jp</p><br>',
-    'パスワード：<p><input type="password" id="pass"></p><br>',
-    '<input type="button" value="ログイン" onclick="login()">',
+    '<p>こんにちは</p>',
     '<div>'
   ]
   let element = document.getElementById('user_show');
   element.remove();
-  
+  let cookie = {};
+  cookie_get(cookie);
+  let date = new Date();
+  let time = date.getFullYear() * 365 * 24 * 60 + (date.getMonth() + 1) * 30 * 24 * 60 + date.getDate() * 24 * 60 + date.getHours() * 60 + date.getMinutes();
+  if (time - cookie[time] > 30){
+    var n = html1;
+  }else{
+    var n = html2;
+  }
   let element = document.getElementById('main');
-  
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < n.length; i++) {
     element.insertAdjacentHTML('beforeend', n[i]);
   }
-  
-  
   /*
   beforebegin	指定した要素の直前に挿入する
   afterbegin	指定した要素内にある最初の子要素の前に挿入する
